85c734923b7e827cd16691c2f6f7f63a6ad81095
suprakit <suprakitonnuch@gmail.com>
2021-07-19 23:22:55 +0700

    Added Help Command

--- src/main/java/io/muzoo/ssc/zork/command/HelpCommand.java
+++ src/main/java/io/muzoo/ssc/zork/command/HelpCommand.java
@@ -1,4 +1,47 @@
 package io.muzoo.ssc.zork.command;
 
-public class HelpCommand {
+import io.muzoo.ssc.zork.Game;
+import io.muzoo.ssc.zork.Main;
+
+import java.util.List;
+
+public class HelpCommand implements Command {
+    /**
+     * Help text output.
+     */
+    private final String helpText = "\n"
+            + "===========================\n"
+            + "   help - Print out all command.\n"
+            + "===========================\n"
+            + "   exit - Exit game.\n"
+            + "   play {map name} - Load to the map\n"
+            + "   load {load saved game} - Load from save file\n"
+            + "========================\n"
+            + "      In-game Command\n"
+            + "========================\n"
+            + "   info - Give players information and currently room.\n"
+            + "   take {item} - Pick up item.\n"
+            + "   drop {item} - Drop an item.\n"
+            + "   attack with {item} - Attack with an item.\n"
+            + "   go {direction} - Move to ( north, south, east, west ).\n"
+            + "   map - map.\n"
+            + "   autopilot {file} - Run the game by autopilot mode.\n"
+            + "   quit - Exit.\n"
+            + "   save {name} - Save game.\n"
+            + "========================\n";
+
+    @Override
+    public int numArgs() {
+        return 0;
+    }
+
+    @Override
+    public String getCommand() {
+        return null;
+    }
+
+    @Override
+    public void execute(Game game, List<String> args) {
+        System.out.print(helpText);
+    }
 }
